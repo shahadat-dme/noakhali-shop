dbc01e4f74be2319ae10e29bcaa2464b44127481
Shahadat <shahadat2cp17@gmail.com>
2021-12-12 11:03:07 +0600

    Navbar

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Announcement = () => {
+    return (
+        <Container>
+            Super Deal! Free Shipping on Orders Over $50
+        </Container>
+    )
+}
+
+export default Announcement
