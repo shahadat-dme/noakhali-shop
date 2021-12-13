4472ee6d0b17ae437afc9303f4b571a87f78747a
Shahadat <shahadat2cp17@gmail.com>
2021-12-13 19:23:40 +0600

    full

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Product from "./pages/Product";
-// import Home from "./pages/Home";
+// import Product from "./pages/Product";
+import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
 // import Register from "./pages/Register";
 // import Login from "./pages/Login";
@@ -7,9 +7,11 @@ import Product from "./pages/Product";
 
 const App = () => {
   return (
-    // <Home/>
-    // <ProductList/>
-    <Product/>
+    <>
+    <Home/>
+    {/* <ProductList/> */}
+    {/* <Product/> */}
+    </>
   )
 };
 
