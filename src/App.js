8266d67cfe5b5fdc12c028576b549e4cdb3df12a
Shahadat <shahadat2cp17@gmail.com>
2021-12-13 15:16:55 +0600

    ProdunctList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-
-// import './App.css';
-
+// import Product from "./pages/Product";
 import Home from "./pages/Home";
+// import ProductList from "./pages/ProductList";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import Cart from "./pages/Cart";
 
-function App() {
-  return (
-    <div className="App">
-      <Home/>
-    </div>
-  );
-}
+const App = () => {
+  return <Home/>;
+};
 
 export default App;
